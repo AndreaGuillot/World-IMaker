c4da945605a382bbae4f8303bd1e843857ea9e9d
Andréa Guillot <gllt.andrea@gmail.com>
2019-12-26 14:11:17 +0100

    ImGui : select color

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -43,10 +43,45 @@ void Interface::editCube(Cubes &cube, Curseur &cursor) const
 {
 	ImGui::SetNextWindowPos(ImVec2(10, 10));
     
+	std::vector<glm::vec4> allColors = {glm::vec4(0.8, 0.2, 0.3, 1.0), glm::vec4(0.3, 0.8, 0.2, 1.0), glm::vec4(0.2, 0.3, 0.8, 1.0)};
 	glm::vec4 defaultColor = glm::vec4(1.0, 1.0, 1.0, 1.0);
 
 	ImGui::Begin("Options : Cube / Colonne");
 	{
+		ImGui::Text("Choisir la couleur du cube :");
+		
+        static int selected = -1;
+		char buf[32];
+		{
+			// color 1
+            sprintf(buf, "%d - Rouge", 1);
+            if (ImGui::Selectable(buf, selected == 0))
+            {
+                selected = 0;
+                cube.editColor(cursor.getPosition(), allColors[0]);
+            }
+            // color 2
+            sprintf(buf, "%d - Vert", 2);
+            if (ImGui::Selectable(buf, selected == 1))
+            {
+                selected = 1;
+                cube.editColor(cursor.getPosition(), allColors[1]);
+            }
+            // color 3
+            sprintf(buf, "%d - Bleu", 3);
+            if (ImGui::Selectable(buf, selected == 2))
+            {
+                selected = 2;
+                cube.editColor(cursor.getPosition(), allColors[2]);
+            }
+
+            if(selected == 0) defaultColor = allColors[0];
+            if(selected == 1) defaultColor = allColors[1];
+            if(selected == 2) defaultColor = allColors[2];
+		}
+
+		ImGui::Text(" ");
+
 		ImGui::Text("Action pour un cube :");
 		{
 			if (ImGui::Button("  Créer cube   "))
@@ -61,6 +96,8 @@ void Interface::editCube(Cubes &cube, Curseur &cursor) const
 				cube.removeCube(cursor.getPosition());
 			}
 		}
+
+		ImGui::Text(" ");
 		
 		ImGui::Text("Action sur une colonne :");
 		{
@@ -74,21 +111,13 @@ void Interface::editCube(Cubes &cube, Curseur &cursor) const
 
 			ImGui::SameLine();
 
-			{
-				if (ImGui::Button("  Dig  colonne  ")) {
-					if (cube.findCube(cursor.getPosition()) != -1) {
-						cube.digCube(cursor.getPosition());
-					}
+			if (ImGui::Button("  Dig  colonne  ")) {
+				if (cube.findCube(cursor.getPosition()) != -1) {
+					cube.digCube(cursor.getPosition());
 				}
 			}
 		}
-		
-		ImGui::Text("Choisir la couleur du cube :");
-		{
-			if (ImGui::Button("Modifier couleur")) {
-				cube.editColor(cursor.getPosition(), defaultColor);
-			}
-		}
+
 	}
 	ImGui::End();
 }
