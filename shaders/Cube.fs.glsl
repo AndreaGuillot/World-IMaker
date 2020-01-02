ddd32c8a5bc3cd7601a357655c01b65ab17c914c
Line <line.jpn25@gmail.com>
2020-01-02 18:48:12 +0100

    2 lumières ponctuelles non modifiable

--- shaders/Cube.fs.glsl
+++ shaders/Cube.fs.glsl
@@ -8,6 +8,7 @@ in vec4 vColor;
 
 // Uniform
 uniform vec3 uLightDir;
+uniform vec3 uLightPos;
 uniform sampler2D uTexture;
 
 // Output
@@ -17,11 +18,22 @@ void main()
 {
 	// calcul light
 	vec3 dir = normalize(vec3(0.5, -0.7, -0.8));
-	float luminosite = max(-dot(vNormal, dir), 0.2);
+	vec3 pos = normalize(vec3(0.8, -0.5, -0.1));
+	vec3 pos2 = normalize(vec3(-0.2, 0.2, -0.8));
+
+	vec3 dirBetweenPointLightAndCurrentPixel = normalize(vFragPosition-pos);
+	vec3 dirBetweenPointLightAndCurrentPixel2 = normalize(vFragPosition-pos2);
+	
+	float luminosityDirLight = max(-dot(vNormal, dir), 0.2);
+	float luminosityPointLight = max(-dot(vNormal, dirBetweenPointLightAndCurrentPixel ), 0.2);
+	float luminosityPointLight2 = max(-dot(vNormal, dirBetweenPointLightAndCurrentPixel2 ), 0.2);
+
+	float totalLuminosity = min(luminosityDirLight + luminosityPointLight, 1.);
+	float totalLuminosity2 = min(luminosityPointLight2 + luminosityPointLight, 1.);
 
 	if(vColor != vec4(1.0, 1.0, 1.0, 1.0))
 	{
-		fFragColor = vColor * luminosite;
+		fFragColor = vec4(vColor * totalLuminosity2); 
 	}
 	
 /*
